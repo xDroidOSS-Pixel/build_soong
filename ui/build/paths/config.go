4e11c18a277336a7e8b3b2319fd0548bc5f1a4f4
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-13 15:13:13 +0800

    build: whitelist repo
    15:11:58 Disallowed PATH tool "repo" used: []string{"repo", "list", "--path"}
    15:11:58 See https://android.googlesource.com/platform/build/+/master/Changes.md
    PATH_Tools for more information.
    
    Change-Id: I95d3b4accfb8f84a47ce50bbe171c4a5b66ff96b
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -101,6 +101,7 @@ var Configuration = map[string]PathConfig{
 	"lsof":    Allowed,
 	"openssl": Allowed,
 	"pstree":  Allowed,
+	"repo":   Allowed,
 	"rsync":   Allowed,
 	"sh":      Allowed,
 	"stubby":  Allowed,
