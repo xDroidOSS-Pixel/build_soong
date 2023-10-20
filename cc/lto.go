f2bef2b9dd4a917e2b50133f9c11a043f7f8e646
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-20 21:43:37 +0800

    lto: Optimize compiler flags
    reference: https://github.com/yaap/build_soong/commits/f6c69aa0b60dd1a7a47cdfe948f4c6fae186ef1d/cc/lto.go
    
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Change-Id: Iec4e39588165be23300a260c8506df7b882ee880
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- cc/lto.go
+++ cc/lto.go
@@ -136,6 +136,25 @@ func (lto *lto) flags(ctx BaseModuleContext, flags Flags) Flags {
 			flags.Local.LdFlags = append(flags.Local.LdFlags,
 				"-Wl,-plugin-opt,-import-instr-limit=5")
 		}
+		
+        // Reduce the inlining threshold for a better balance of binary size and
+        // performance.
+        additionalLdFlags := []string{
+            "-Wl,-mllvm,-inline-threshold=600",
+            "-Wl,-mllvm,-inlinehint-threshold=550",
+            "-Wl,-mllvm,-unroll-threshold=800",
+            "-Wl,-mllvm,-polly",
+            "-Wl,-mllvm,-polly-ast-use-context",
+            "-Wl,-mllvm,-polly-invariant-load-hoisting",
+            "-Wl,-mllvm,-polly-vectorizer=stripmine",
+            "-Wl,-mllvm,-polly-loopfusion-greedy=1",
+            "-Wl,-mllvm,-polly-scheduling-chunksize=1",
+            "-Wl,-plugin-opt,-import-instr-limit=40",
+            "-Wl,-plugin-opt=O3",
+            "-Wl,-O3",
+            "-Wl,--gc-sections",
+        }
+        flags.Local.LdFlags = append(flags.Local.LdFlags, additionalLdFlags...)
 	}
 	return flags
 }
